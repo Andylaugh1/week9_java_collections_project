ead63c0e34c4ba449505130af0a0de3c60378c51
Andy Laughlin <atl11384@gmail.com>
2018-06-01 13:32:05 +0100

    Initial tests added for item class. Get make and get model

--- src/test/java/DrinkTest.java
+++ src/test/java/DrinkTest.java
@@ -10,11 +10,21 @@ public class DrinkTest {
 
     @Before
     public void before() {
-        drink = new Drink("Smirnoff", "Red Label", "05/30/2017", 20.00, 2.00, 21.00, DrinkType.VODKA);
+        drink = new Drink("Smirnoff", "Red Label", "30/05/2017", 20.00, 2.00, 21.00, DrinkType.VODKA);
     }
 
     @Test
     public void canGetMake() {
         assertEquals("Smirnoff", drink.getMake());
+    }
+    
+    @Test
+    public void canGetModel() {
+        assertEquals("Red Label", drink.getModel());
+    }
+
+    @Test
+    public void canGetPurchaseDate() {
+        assertEquals("30/05/2017", drink.getPurchaseDate());
     }
 }
