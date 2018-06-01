98a1ba554544b9383a8a583a3fa5af805c437958
Andy Laughlin <atl11384@gmail.com>
2018-06-01 14:08:34 +0100

    Collection class added

--- src/main/java/Collection.java
+++ src/main/java/Collection.java
@@ -2,10 +2,15 @@ import java.util.ArrayList;
 
 public class Collection {
 
+    private String name;
     private ArrayList<Item> items;
 
-    public Collection(ArrayList items) {
-        this.items = items;
+
+    public Collection(String name, ArrayList items) {
+        this.name = name;
+        this.items = new ArrayList<>();
     }
+
+
 
 }
