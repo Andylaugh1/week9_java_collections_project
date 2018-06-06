9ef3964846d9fa75915f8f21355dafdd75c4ac4e
Andy Laughlin <atl11384@gmail.com>
2018-06-06 13:53:02 +0100

    Refactor of collectionTest file

--- src/test/java/CollectionTest.java
+++ src/test/java/CollectionTest.java
@@ -6,54 +6,54 @@ import static org.junit.Assert.assertEquals;
 
 public class CollectionTest {
 
-    Collection collection;
-    Item item, item1;
+    Collection drinkCollection;
+    Drink drink, drink1;
 
     @Before
     public void before() {
-        collection = new Collection();
-        item = new Drink("Smirnoff", "Red Label",2012, 20.00, 2.00, DrinkType.VODKA);
-        item1 = new Drink("Gordon's", "Gordon's",2013, 17.00, 1.50, DrinkType.GIN);
+        drinkCollection = new Collection();
+        drink = new Drink("Smirnoff", "Red Label",2012, 20.00, 2.00, DrinkType.VODKA);
+        drink1 = new Drink("Gordon's", "Gordon's",2013, 17.00, 1.50, DrinkType.GIN);
     }
 
 
     @Test
     public void canCountItems() {
-        assertEquals(0, collection.countItems());
+        assertEquals(0, drinkCollection.countItems());
     }
 
     @Test
     public void canAddAnItemToCollection() {
-        collection.addItem(item);
-        assertEquals(1, collection.countItems());
+        drinkCollection.addItem(drink);
+        assertEquals(1, drinkCollection.countItems());
     }
 
     @Test
     public void canRemoveItemFromCollection() {
-        collection.addItem(item);
-        collection.removeItem(item);
-        assertEquals(0, collection.countItems());
+        drinkCollection.addItem(drink);
+        drinkCollection.removeItem(drink);
+        assertEquals(0, drinkCollection.countItems());
     }
 
     @Test
     public void canGetTotalPricePaidForItems() {
-        collection.addItem(item1);
-        collection.addItem(item);
-        assertEquals(37.00, collection.calculateTotalItemPricePaid(), 0.01);
+        drinkCollection.addItem(drink1);
+        drinkCollection.addItem(drink);
+        assertEquals(37.00, drinkCollection.calculateTotalItemPricePaid(), 0.01);
     }
 
     @Test
     public void canGetTotalPricePaidForShipping() {
-        collection.addItem(item1);
-        collection.addItem(item);
-        assertEquals(3.50, collection.calculateTotalShippingPricePaid(), 0.01);
+        drinkCollection.addItem(drink1);
+        drinkCollection.addItem(drink);
+        assertEquals(3.50, drinkCollection.calculateTotalShippingPricePaid(), 0.01);
     }
 
     @Test
     public void canGetTotalPriceShippingAndBuyingCombined() {
-        collection.addItem(item1);
-        collection.addItem(item);
-        assertEquals(40.50, collection.calculateTotalPricePaid(), 0.01);
+        drinkCollection.addItem(drink1);
+        drinkCollection.addItem(drink);
+        assertEquals(40.50, drinkCollection.calculateTotalPricePaid(), 0.01);
     }
 
 
